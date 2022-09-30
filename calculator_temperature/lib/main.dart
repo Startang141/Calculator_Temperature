192634d45775f9e36309e38093b5db0093caa829
startang141m <bintangadiyatma141@gmail.com>
2022-09-30 20:16:38 +0700

    History

--- calculator_temperature/lib/main.dart
+++ calculator_temperature/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,21 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Calculator Temperature',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Calculator Temperature'),
@@ -32,15 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,40 +31,116 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var items = ['Kelvin', 'Reamor', 'Farenheit', 'Semua'];
+
+  String dropdownvalue = 'Kelvin';
+  String? selectedValue;
   double celcius = 0;
   double kelvin = 0;
   double reamor = 0;
+  double farenheit = 0;
+  double output = 0;
+  double output1 = 0;
+  double output2 = 0;
+  double output3 = 0;
+  List<String> listViewItem1 = <String>[];
+  List<String> listViewItem2 = <String>[];
+  List<String> listViewItem3 = <String>[];
+  List<String> listViewItem4 = <String>[];
   TextEditingController inputTemperature = TextEditingController();
 
   konversi() {
     setState(() {
       celcius = double.parse(inputTemperature.text);
-      reamor = 4 / 5 * celcius;
-      kelvin = 273.15 + celcius;
+      if (dropdownvalue == 'Kelvin') {
+        output = 273.15 + celcius;
+        reamor = 0;
+        farenheit = 0;
+        kelvin = output;
+      } else if (dropdownvalue == 'Reamor') {
+        output = 4 / 5 * celcius;
+        reamor = output;
+        farenheit = 0;
+        kelvin = 0;
+      } else if (dropdownvalue == 'Farenheit') {
+        output = (9 / 5 * celcius) + 32;
+        reamor = 0;
+        farenheit = output;
+        kelvin = 0;
+      } else {
+        output1 = 273.15 + celcius;
+        output2 = 4 / 5 * celcius;
+        output3 = (9 / 5 * celcius) + 32;
+        reamor = output2;
+        farenheit = output3;
+        kelvin = output1;
+      }
+      listViewItem1.add('$kelvin');
+      listViewItem2.add('$reamor');
+      listViewItem3.add('$farenheit');
+      listViewItem4.add('$celcius');
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
-        body: Container(
-            child: Column(
-          children: [InputanSuhu(), HasilKonversi(), ButtonKonversi()],
-        )));
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      child: InputanSuhu(),
+                    ),
+                    Container(
+                      child: dropDown(),
+                    ),
+                    Container(
+                      child: hasilKonversi(),
+                    ),
+                    Container(
+                      child: buttonKonversi(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const Expanded(
+              flex: 1,
+              child: Text('Riwayat Konversi'),
+            ),
+            //const SizedBox(),
+            Expanded(flex: 2, child: riwayatKonversi()),
+          ],
+        ));
   }
 
-  SizedBox ButtonKonversi() {
+  dropDown() {
+    return DropdownButton(
+      value: dropdownvalue,
+      icon: const Icon(Icons.keyboard_arrow_down),
+      items: items.map((String items) {
+        return DropdownMenuItem(
+          value: items,
+          child: Text(items),
+        );
+      }).toList(),
+      onChanged: (String? newValue) {
+        setState(() {
+          dropdownvalue = newValue!;
+        });
+      },
+    );
+  }
+
+  buttonKonversi() {
     return SizedBox(
         width: double.infinity,
         child: ElevatedButton(
@@ -90,7 +149,7 @@ class _MyHomePageState extends State<MyHomePage> {
         ));
   }
 
-  Row HasilKonversi() {
+  hasilKonversi() {
     return Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
       Column(
         children: [
@@ -101,6 +160,19 @@ class _MyHomePageState extends State<MyHomePage> {
           const SizedBox(height: 20),
           Text(
             "$reamor",
+            style: const TextStyle(fontSize: 25),
+          )
+        ],
+      ),
+      Column(
+        children: [
+          const Text(
+            'Suhu dalam Farenheit',
+            style: TextStyle(fontSize: 15),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            "$farenheit",
             style: const TextStyle(fontSize: 25),
           )
         ],
@@ -121,7 +193,7 @@ class _MyHomePageState extends State<MyHomePage> {
     ]);
   }
 
-  Container InputanSuhu() {
+  InputanSuhu() {
     return Container(
         padding: EdgeInsets.all(10),
         child: TextFormField(
@@ -131,5 +203,46 @@ class _MyHomePageState extends State<MyHomePage> {
               border: UnderlineInputBorder(),
               labelText: 'Masukan Suhu Dalam Celcius',
             )));
+  }
+
+  riwayatKonversi() {
+    return SizedBox(
+      //height: 100,
+      child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: listViewItem1.length,
+          itemBuilder: (context, index) => Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.black)),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: const [
+                            Text('Kelvin\t'),
+                            Text('Reamur\t'),
+                            Text('Farenheit\t'),
+                            Text('Celcius\t'),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(listViewItem1.elementAt(index)),
+                            Text(listViewItem2.elementAt(index)),
+                            Text(listViewItem3.elementAt(index)),
+                            Text(listViewItem4.elementAt(index)),
+                          ],
+                        )
+                      ],
+                    ))
+                  ],
+                ),
+              )),
+    );
   }
 }
